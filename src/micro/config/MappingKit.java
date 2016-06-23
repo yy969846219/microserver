fb1f9285f12a829b68bf106446bd1c353dded66d
yy969846219 <15908806961@139.com>
2016-06-23 11:36:07 +0800

    第一次导入

---
+++ src/micro/config/MappingKit.java
@@ -0,0 +1,14 @@
+package micro.config;
+
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+
+/**
+ * model和数据库表映射
+ */
+public class MappingKit {
+	
+	public static void mapping(ActiveRecordPlugin arp) {
+		
+	}
+
+}
